4ff68701c7654109edf36170e25d6c92424ce789
Matt Ziegler <mattzig@cs.washington.edu>
2020-04-20 18:04:16 -0500

    adjust ave map centering

--- map-ave.js
+++ map-ave.js
@@ -63,7 +63,7 @@ const ALL_TAGS_GROUPED = [
 ]
 
 
-let map = L.map('map').setView([ 47.65822,-122.313383], 18)
+let map = L.map('map').setView([ 47.658436,-122.312699], 18)
 
 // Background layer
 //let basemap = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
